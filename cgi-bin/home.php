28475d40ae86dc67eec6daf62e519b01662f80b1
Constanza Csori Pinto <coni.csorip@gmail.com>
2014-06-19 18:35:35 -0400

    Change view in home.php
    Add the css for view in home.php

--- cgi-bin/home.php
+++ cgi-bin/home.php
@@ -11,42 +11,117 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>ArchLearn - Home</title>
+		<link href="css/bootstrapHome.css" rel="stylesheet">
+    	<style type="text/css">
+      body {
+        padding-top: 20px;
+        padding-bottom: 60px;
+      }
+
+      /* Custom container */
+      .container {
+        margin: 0 auto;
+        max-width: 1000px;
+      }
+      .container > hr {
+        margin: 60px 0;
+      }
+
+      /* Main marketing message and sign up button */
+      .jumbotron {
+        margin: 80px 0;
+        text-align: center;
+      }
+      .jumbotron h1 {
+        font-size: 100px;
+        line-height: 1;
+      }
+      .jumbotron .lead {
+        font-size: 24px;
+        line-height: 1.25;
+      }
+      .jumbotron .btn {
+        font-size: 21px;
+        padding: 14px 24px;
+      }
+
+      /* Supporting marketing content */
+      .marketing {
+        margin: 60px 0;
+      }
+      .marketing p + h4 {
+        margin-top: 28px;
+      }
+
+
+      /* Customize the navbar links to be fill the entire space of the .navbar */
+      .navbar .navbar-inner {
+        padding: 0;
+      }
+      .navbar .nav {
+        margin: 0;
+        display: table;
+        width: 100%;
+      }
+      .navbar .nav li {
+        display: table-cell;
+        width: 1%;
+        float: none;
+      }
+      .navbar .nav li a {
+        font-weight: bold;
+        text-align: center;
+        border-left: 1px solid rgba(255,255,255,.75);
+        border-right: 1px solid rgba(0,0,0,.1);
+      }
+      .navbar .nav li:first-child a {
+        border-left: 0;
+        border-radius: 3px 0 0 3px;
+      }
+      .navbar .nav li:last-child a {
+        border-right: 0;
+        border-radius: 0 3px 3px 0;
+      }
+    </style>
+    	<link href="css/bootstrap-responsiveHome.css" rel="stylesheet">
+
 	</head>
 	<body>
-		Bienvenid@ <?php echo $_SESSION['name']." ".$_SESSION['last_name'];?>.<br>
-		<a href="login/session_manager.php?logout=yes">cerrar sesión</a>
-		<h1>TravelSyst</h1>
-		<table border='1' width='800'>
-			<tr>
-				<td width='200'>
-					<h2>Menú</h2>
-				</td>
-				<td>
-					<h2>Novedades</h2>
-				</td>
-			</tr>
-			<tr>
-				<td>
-					<div class="menu">
-						<ol>
-							<?php
-								echo "<li><a href='info/profile.php'>Profile</a>";
-								echo "<li><a href='course/new_course.php'>Start Course</a>";
-								echo "<li><a href='course/continue.php'>Continue Course</a>";
-								echo "<li><a href='info/history.php'>History</a>";
-								if(isset($_SESSION['type']) && $_SESSION['type'] == 1){
-									echo "<li><a href='administrate.php'>Administrar</a>";
-								}
-							?>		
-						</ol>
-					</div>
-				</td>
-				<td>
-					<b>Hemos inaugurado nuestro sitio!</b>
-					Bienvenido a nuestro sitio, le traemos lo mejor de la Tierra Media para planificar su viaje. Saludos
-				</td>
-			</tr>
-		</table>
-		<br><br>
+
+		<div class="container">
+
+      <div class="masthead">
+        <h3 class="muted">Bienvenid@ <?php echo $_SESSION['name']." ".$_SESSION['last_name'];?>. </h3>
+        <h3 class="muted"> <a href="login/session_manager.php?logout=yes">cerrar sesión</a> </h3>
+        <div class="navbar">
+          <div class="navbar-inner">
+            <div class="container">
+              <ul class="nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#">Profile</a></li>
+                <li><a href="#">Start new course</a></li>
+                <li><a href="#">Continue course</a></li>
+                <li><a href="#">Records</a></li>
+                <li><a href="<?php if(isset($_SESSION['type']) && $_SESSION['type'] == 1){ echo "administrate.php" else {echo "#"}}?>">Administrate</a></li>
+              </ul>
+            </div>
+          </div>
+        </div><!-- /.navbar -->
+      </div>
+
+      <!-- Jumbotron -->
+      <div class="jumbotron">
+        <h1>Novedades</h1>
+        <p class="lead">We are up and running. Welcome to our site !</p>
+      </div>
+
+      <hr>
+
+      <div class="footer">
+        <p>&copy; Computer Arquitecture 2014</p>
+      </div>
+
+    </div>
+		<br>
 	</body>
 </html>
