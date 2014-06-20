e3588b5659e49eee45ec65f3098911087d65656b
Matias Pardo G <mat.pardo@gmail.com>
2014-06-20 01:11:34 -0400

    video

--- cgi-bin/experiences/types/res_types/ResourceVideo.php
+++ cgi-bin/experiences/types/res_types/ResourceVideo.php
@@ -1,2 +1,12 @@
 <?php
+	include_once('db/archlearn_dbconfig.php');
+	$db = DbConfig::getConnection();
+
+	function generateResourceCode($id){
+		$sql = sprintf("SELECT idVideo, link 
+			FROM resourcevideo WHERE idExperience=%d",$id);
+		$result = $db->query($sql);
+		$row = $result->fetch_assoc();
+		echo "<iframe width='560' height='315' src='".$row['link']"' frameborder='0' allowfullscreen></iframe>";
+	}
 ?>
